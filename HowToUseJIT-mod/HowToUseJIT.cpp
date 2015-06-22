f236473654059a65fdd88ee9d4e9aeea513ba234
ypsitau <ypsitau@nifty.com>
2015-06-22 10:52:32 +0900

    updated HowToJIT

--- HowToUseJIT-mod/HowToUseJIT.cpp
+++ HowToUseJIT-mod/HowToUseJIT.cpp
@@ -47,44 +47,42 @@
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
 
-using namespace llvm;
-
 int main()
 {
-	InitializeNativeTarget();
+	llvm::InitializeNativeTarget();
 
-	LLVMContext Context;
+	llvm::LLVMContext Context;
   
 	// Create some module to put our function into it.
-	std::unique_ptr<Module> Owner = make_unique<Module>("test", Context);
-	Module *M = Owner.get();
+	std::unique_ptr<llvm::Module> Owner = llvm::make_unique<llvm::Module>("test", Context);
+	llvm::Module *M = Owner.get();
 
 	// Create the add1 function entry and insert this entry into module M.  The
 	// function will have a return type of "int" and take an argument of "int".
 	// The '0' terminates the list of argument types.
-	Function *Add1F =
-		cast<Function>(M->getOrInsertFunction("add1", Type::getInt32Ty(Context),
-											  Type::getInt32Ty(Context),
-											  (Type *)0));
+	llvm::Function *Add1F =
+		llvm::cast<llvm::Function>(M->getOrInsertFunction("add1", llvm::Type::getInt32Ty(Context),
+													llvm::Type::getInt32Ty(Context),
+													(llvm::Type *)0));
 
 	// Add a basic block to the function. As before, it automatically inserts
 	// because of the last argument.
-	BasicBlock *BB = BasicBlock::Create(Context, "EntryBlock", Add1F);
+	llvm::BasicBlock *BB = llvm::BasicBlock::Create(Context, "EntryBlock", Add1F);
 
 	// Create a basic block builder with default parameters.  The builder will
 	// automatically append instructions to the basic block `BB'.
-	IRBuilder<> builder(BB);
+	llvm::IRBuilder<> builder(BB);
 
 	// Get pointers to the constant `1'.
-	Value *One = builder.getInt32(1);
+	llvm::Value *One = builder.getInt32(1);
 
 	// Get pointers to the integer argument of the add1 function...
 	assert(Add1F->arg_begin() != Add1F->arg_end()); // Make sure there's an arg
-	Argument *ArgX = Add1F->arg_begin();  // Get the arg
+	llvm::Argument *ArgX = Add1F->arg_begin();  // Get the arg
 	ArgX->setName("AnArg");            // Give it a nice symbolic name for fun.
 
 	// Create the add instruction, inserting it into the end of BB.
-	Value *Add = builder.CreateAdd(One, ArgX);
+	llvm::Value *Add = builder.CreateAdd(One, ArgX);
 
 	// Create the return instruction and add it to the basic block
 	builder.CreateRet(Add);
@@ -94,40 +92,40 @@ int main()
 
 	// Now we're going to create function `foo', which returns an int and takes no
 	// arguments.
-	Function *FooF =
-		cast<Function>(M->getOrInsertFunction("foo", Type::getInt32Ty(Context),
-											  (Type *)0));
+	llvm::Function *FooF =
+		llvm::cast<llvm::Function>(M->getOrInsertFunction("foo", llvm::Type::getInt32Ty(Context),
+													(llvm::Type *)0));
 
 	// Add a basic block to the FooF function.
-	BB = BasicBlock::Create(Context, "EntryBlock", FooF);
+	BB = llvm::BasicBlock::Create(Context, "EntryBlock", FooF);
 
 	// Tell the basic block builder to attach itself to the new basic block
 	builder.SetInsertPoint(BB);
 
 	// Get pointer to the constant `10'.
-	Value *Ten = builder.getInt32(10);
+	llvm::Value *Ten = builder.getInt32(10);
 
 	// Pass Ten to the call to Add1F
-	CallInst *Add1CallRes = builder.CreateCall(Add1F, Ten);
+	llvm::CallInst *Add1CallRes = builder.CreateCall(Add1F, Ten);
 	Add1CallRes->setTailCall(true);
 
 	// Create the return instruction and add it to the basic block.
 	builder.CreateRet(Add1CallRes);
 
 	// Now we create the JIT.
-	ExecutionEngine* EE = EngineBuilder(std::move(Owner)).create();
+	llvm::ExecutionEngine* EE = llvm::EngineBuilder(std::move(Owner)).create();
 
-	outs() << "We just constructed this LLVM module:\n\n" << *M;
-	outs() << "\n\nRunning foo: ";
-	outs().flush();
+	llvm::outs() << "We just constructed this LLVM module:\n\n" << *M;
+	llvm::outs() << "\n\nRunning foo: ";
+	llvm::outs().flush();
 
 	// Call the `foo' function with no arguments:
-	std::vector<GenericValue> noargs;
-	GenericValue gv = EE->runFunction(FooF, noargs);
+	std::vector<llvm::GenericValue> noargs;
+	llvm::GenericValue gv = EE->runFunction(FooF, noargs);
 
 	// Import result of execution:
-	outs() << "Result: " << gv.IntVal << "\n";
+	llvm::outs() << "Result: " << gv.IntVal << "\n";
 	delete EE;
-	llvm_shutdown();
+	llvm::llvm_shutdown();
 	return 0;
 }
