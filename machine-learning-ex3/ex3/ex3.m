28eb517183a0a3ed232c1e2de9e478b447758a0a
ypsitau <ypsitau@nifty.com>
2017-03-03 11:51:48 +0900

    corrected comment marker and indentation

--- machine-learning-ex3/ex3/ex3.m
+++ machine-learning-ex3/ex3/ex3.m
@@ -1,41 +1,41 @@
 %% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
 
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
+%%  Instructions
+%%  ------------
+%%
+%%  This file contains code that helps you get started on the
+%%  linear exercise. You will need to complete the following functions
+%%  in this exericse:
+%%
+%%     lrCostFunction.m (logistic regression cost function)
+%%     oneVsAll.m
+%%     predictOneVsAll.m
+%%     predict.m
+%%
+%%  For this exercise, you will not need to change any code in this file,
+%%  or any other files other than those mentioned above.
+%%
 
 %% Initialization
 clear ; close all; clc
 
 %% Setup the parameters you will use for this part of the exercise
-input_layer_size  = 400;  % 20x20 Input Images of Digits
-num_labels = 10;          % 10 labels, from 1 to 10
-                          % (note that we have mapped "0" to label 10)
+input_layer_size  = 400;  %% 20x20 Input Images of Digits
+num_labels = 10;          %% 10 labels, from 1 to 10
+%% (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset.
-%  You will be working with a dataset that contains handwritten digits.
-%
+%%  We start the exercise by first loading and visualizing the dataset.
+%%  You will be working with a dataset that contains handwritten digits.
+%%
 
-% Load Training Data
+%% Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
-load('ex3data1.mat'); % training data stored in arrays X, y
+load('ex3data1.mat'); %% training data stored in arrays X, y
 m = size(X, 1);
 
-% Randomly select 100 data points to display
+%% Randomly select 100 data points to display
 rand_indices = randperm(m);
 sel = X(rand_indices(1:100), :);
 
@@ -45,14 +45,14 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ============ Part 2a: Vectorize Logistic Regression ============
-%  In this part of the exercise, you will reuse your logistic regression
-%  code from the last exercise. You task here is to make sure that your
-%  regularized logistic regression implementation is vectorized. After
-%  that, you will implement one-vs-all classification for the handwritten
-%  digit dataset.
-%
+%%  In this part of the exercise, you will reuse your logistic regression
+%%  code from the last exercise. You task here is to make sure that your
+%%  regularized logistic regression implementation is vectorized. After
+%%  that, you will implement one-vs-all classification for the handwritten
+%%  digit dataset.
+%%
 
-% Test case for lrCostFunction
+%% Test case for lrCostFunction
 fprintf('\nTesting lrCostFunction()');
 
 theta_t = [-2; -1; 1; 2];
