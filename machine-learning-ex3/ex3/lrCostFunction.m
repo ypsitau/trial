9d53e21d7bd3fa22dc41796aad7d8dbe44324967
ypsitau <ypsitau@nifty.com>
2017-03-03 12:43:56 +0900

    updated exercises

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,21 +36,28 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
   %%           grad = grad + YOUR_CODE_HERE (using the temp variable)
   %%
 
-  %% X = [x1(1)  x2(1)  x3(1)]  y = [y(1)]
-  %%     |x1(2)  x2(2)  x3(2)|      |y(2)|
-  %%     |x1(3)  x2(3)  x3(3)|      |y(3)|
-  %%     |x1(4)  x2(4)  x3(4)|      |y(4)|
-  %%     [x1(5)  x2(5)  x3(5)]      [y(5)]
+  %% X = [x0(1)  x1(1)  x2(1)  x3(1)]  y = [y(1)]
+  %%     |x0(2)  x1(2)  x2(2)  x3(2)|      |y(2)|
+  %%     |x0(3)  x1(3)  x2(3)  x3(3)|      |y(3)|
+  %%     |x0(4)  x1(4)  x2(4)  x3(4)|      |y(4)|
+  %%     [x0(5)  x1(5)  x2(5)  x3(5)]      [y(5)]
 
-  %% theta = [t1]
+  %% theta = [t0]
+  %%         |t1|
   %%         |t2|
   %%         [t3]
 
-  h = sigmoid(X * theta)
-  J = (sum(-y .* log(h) - 1 - y) .* log(1 - h)) / m
+  theta_t = theta;
+  theta_t(1) = 0;
+  
+  h = sigmoid(X * theta);
+
+  J = sum(-y .* log(h) - (1 - y) .* log(1 - h)) / m;
+  J = J + lambda * sum(theta_t .* theta_t) / (2 * m);
 
   %% =============================================================
 
-  grad = grad(:);
+  grad = ((h - y)' * X)' / m;
+  grad = grad + lambda * theta_t / m;
 
 end
