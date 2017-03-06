7b22f33bcd41c1f45be38bf01c0b97555f7c0139
ypsitau <ypsitau@nifty.com>
2017-03-06 20:38:16 +0900

    updated exercises

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -14,8 +14,26 @@ function p = predictOneVsAll(all_theta, X)
   %% You need to return the following variables correctly 
   p = zeros(size(X, 1), 1);
 
+  %% p = [p(1)   ]
+  %%     |p(2)   |
+  %%     |p(3)   |
+  %%        :
+  %%     [p(5000)]
+
   %% Add ones to the X data matrix
   X = [ones(m, 1) X];
+
+  %% all_theta = [t1,0   t1,1   t1,2   t1,3   ... t1,400 ]  .. one
+  %%             |t2,0   t2,1   t2,2   t2,3   ... t2,400 |  .. two
+  %%             |t3,0   t3,1   t3,2   t3,3   ... t3,400 |  .. three
+  %%                               :
+  %%             [t10,0  t10,1  t10,2  t10,3  ... t10,400]  .. zero
+
+  %% X = [1  x1(1)     x2(1)     x3(1)    ... x400(1)   ]
+  %%     |1  x1(2)     x2(2)     x3(2)    ... x400(2)   |
+  %%     |1  x1(3)     x2(3)     x3(3)    ... x400(3)   |
+  %%                            :
+  %%     [1  x1(5000)  x2(5000)  x3(5000) ... x400(5000)]
 
   %% ====================== YOUR CODE HERE ======================
   %% Instructions: Complete the following code to make predictions using
@@ -30,13 +48,8 @@ function p = predictOneVsAll(all_theta, X)
   %%       for each row.
   %%       
 
-
-
-
-
-
+  [n, p] = max(X * all_theta', [], 2)
 
   %% =========================================================================
-
 
 end
