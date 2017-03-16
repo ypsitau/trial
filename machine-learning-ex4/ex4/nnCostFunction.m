343cf6e7124417154399f36abdce3e71050fc8f3
ypsitau <ypsitau@nifty.com>
2017-03-16 22:06:40 +0900

    updated exercise

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -111,7 +111,6 @@ function [J grad] = nnCostFunction(nn_params, ...
   Theta2_grad = Theta2_grad / m;
 
   Theta1_grad(:, 2 : end) = Theta1_grad(:, 2 : end) + lambda * Theta1(:, 2 : end) / m;
-  
   Theta2_grad(:, 2 : end) = Theta2_grad(:, 2 : end) + lambda * Theta2(:, 2 : end) / m;
 
   %% -------------------------------------------------------------
