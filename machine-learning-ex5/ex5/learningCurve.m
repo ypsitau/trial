953ba079f7281824fa54d9f42891a04b0665a5d9
ypsitau <ypsitau@nifty.com>
2017-03-24 12:11:35 +0900

    updated exercises

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,11 +53,15 @@ function [error_train, error_val] = ...
 
   %% ---------------------- Sample Solution ----------------------
 
+  for i = 1 : m
+    %% Compute train/cross validation errors using training examples 
+    %% X(1:i, :) and y(1:i), storing the result in 
+    %% error_train(i) and error_val(i)
+    t = trainLinearReg(X(1 : i, :), y(1 : i), lambda);
 
-
-
-
-
+	error_train(i) = linearRegCostFunction(X(1 : i, :), y(1 : i), t, 0);
+	error_val(i) = linearRegCostFunction(Xval, yval, t, 0);
+  end
 
   %% -------------------------------------------------------------
 
