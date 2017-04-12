3cb27c9c3f1a3ab9036ce4a1425d39ef8c55e8c7
ypsitau <ypsitau@nifty.com>
2017-04-12 12:11:54 +0900

    im@plemented exercises

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -6,6 +6,28 @@ function Z = projectData(X, U, K)
   %%   the first K columns of U. It returns the projected examples in Z.
   %%
 
+  %% Z = [z_1(1)  z_2(1)  z_K(1)]
+  %%     |z_1(2)  z_2(2)  z_K(2)|
+  %%     |z_1(3)  z_2(3)  z_K(3)|
+  %%     |z_1(4)  z_2(4)  z_K(4)|
+  %%     [z_1(5)  z_2(5)  z_K(5)]
+
+  %% X = [x1(1)  x2(1)  x3(1)  x4(1)]
+  %%     |x1(2)  x2(2)  x3(2)  x4(2)|
+  %%     |x1(3)  x2(3)  x3(3)  x4(3)|
+  %%     |x1(4)  x2(4)  x3(4)  x4(4)|
+  %%     [x1(5)  x2(5)  x3(5)  x4(5)]
+
+  %% U = [u_1_1  u_1_2  u_1_3  u_1_4]
+  %%     |u_2_1  u_2_2  u_2_3  u_2_4]
+  %%     [u_3_1  u_3_2  u_3_3  u_3_4]
+  %%     [u_4_1  u_4_2  u_4_3  u_4_4]
+
+  %% U(:, 1 : K) = [u_1_1  u_1_2  u_1_K]
+  %%               |u_2_1  u_2_2  u_2_K]
+  %%               [u_3_1  u_3_2  u_3_K]
+  %%               [u_4_1  u_4_2  u_4_K]
+
   %% You need to return the following variables correctly.
   Z = zeros(size(X, 1), K);
 
@@ -18,8 +40,7 @@ function Z = projectData(X, U, K)
   %%                    projection_k = x' * U(:, k);
   %%
 
-
-
+  Z = X * U(:, 1 : K);
 
   %% =============================================================
 
--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -6,6 +6,28 @@ function X_rec = recoverData(Z, U, K)
   %%   approximate reconstruction in X_rec.
   %%
 
+  %% Z = [z_1(1)  z_2(1)  z_K(1)]
+  %%     |z_1(2)  z_2(2)  z_K(2)|
+  %%     |z_1(3)  z_2(3)  z_K(3)|
+  %%     |z_1(4)  z_2(4)  z_K(4)|
+  %%     [z_1(5)  z_2(5)  z_K(5)]
+
+  %% X = [x1(1)  x2(1)  x3(1)  x4(1)]
+  %%     |x1(2)  x2(2)  x3(2)  x4(2)|
+  %%     |x1(3)  x2(3)  x3(3)  x4(3)|
+  %%     |x1(4)  x2(4)  x3(4)  x4(4)|
+  %%     [x1(5)  x2(5)  x3(5)  x4(5)]
+
+  %% U = [u_1_1  u_1_2  u_1_3  u_1_4]
+  %%     |u_2_1  u_2_2  u_2_3  u_2_4]
+  %%     [u_3_1  u_3_2  u_3_3  u_3_4]
+  %%     [u_4_1  u_4_2  u_4_3  u_4_4]
+
+  %% U(:, 1 : K) = [u_1_1  u_1_2  u_1_K]
+  %%               |u_2_1  u_2_2  u_2_K]
+  %%               [u_3_1  u_3_2  u_3_K]
+  %%               [u_4_1  u_4_2  u_4_K]
+
   %% You need to return the following variables correctly.
   X_rec = zeros(size(Z, 1), size(U, 1));
 
@@ -21,7 +43,7 @@ function X_rec = recoverData(Z, U, K)
   %%               Notice that U(j, 1:K) is a row vector.
   %%               
 
-
+  X_rec = Z * U(:, 1 : K)';
 
   %% =============================================================
 
