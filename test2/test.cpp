10ea0aa3638c09120faab5187dbce1326c67d29d
ypsitau <ypsitau@nifty.com>
2018-07-19 11:24:01 +0900

    updated

--- test2/test.cpp
+++ test2/test.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+double sub1(bool a, double b)
+{
+	return a * b;
+}
+
+double sub2(bool a, double b)
+{
+	return a? b : 0;
+}
+
 int main()
 {
-	::printf("check\n");
+	for (int i = 0; i < 10; i++) {
+		::printf("%f\n", sub1(i % 2 == 0, i));
+		::printf("%f\n", sub2(i % 2 == 0, i));
+	}
 	return 0;
 }
