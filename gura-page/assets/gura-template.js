7c8eb1fb3955edb2aa5a901a2e8a90ad4508ccef
ypsitau <ypsitau@nifty.com>
2018-07-27 11:48:42 +0900

    updated template

--- gura-page/assets/gura-template.js
+++ gura-page/assets/gura-template.js
@@ -1,10 +1,8 @@
 // jQuery entry
 $(document).ready(function() {
 	$('.list-group-item').on('click', function() {
-		//$(this).text("checked");
-		$('span', this).text("checked");
-		//$('.glyphicon', this)
-		//	.toggleClass('glyphicon-chevron-right')
-		//	.toggleClass('glyphicon-chevron-down');
+		$('.fas', this)
+			.toggleClass('fa-caret-right')
+			.toggleClass('fa-caret-down');
 	});
 });
